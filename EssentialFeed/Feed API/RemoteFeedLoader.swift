1435c0ea44943c37597f8d3dfc3e98af20e1ad50
Wan Lutfi <lutfime_2000@yahoo.com>
2023-02-05 07:49:10 +0800

    Guarantee we not deliver result after `RemoteFeedLoader` deallocated

--- EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -28,8 +28,8 @@ public class RemoteFeedLoader {
     }
     
     public func load(completion: @escaping (Result) -> Void) {
-        client.get(from: url) { result in
-            
+        client.get(from: url) {[weak self] result in
+            guard self != nil else {return}
             
             switch result {
             case .success(let data, let response):
