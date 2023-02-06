4bfeae170b58900b3a0e27d3c82c39cb97c08218
Wan Lutfi <lutfime_2000@yahoo.com>
2023-02-06 10:32:17 +0800

    Make URLProtocolStub intercept every network instead of specific URLs only

--- EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -38,7 +38,7 @@ class URLSessionHTTPClientTests: XCTestCase {
     func test_getFromURL_failsOnRequestError(){
         let url = URL(string: "http://any-url.com")!
         let error = NSError(domain: "any error", code: 1)
-        URLProtocolStub.stub(url: url, data: nil, response: nil, error: error)
+        URLProtocolStub.stub(data: nil, response: nil, error: error)
         
         let sut = URLSessionHTTPClient()
         let exp = expectation(description: "Wait for completion")
@@ -60,7 +60,7 @@ class URLSessionHTTPClientTests: XCTestCase {
 }
 
 private class URLProtocolStub: URLProtocol{
-    private static var stubs = [URL: Stub]()
+    private static var stub: Stub?
     
     private struct Stub{
         let data: Data?
@@ -68,8 +68,8 @@ private class URLProtocolStub: URLProtocol{
         let error: Error?
     }
     
-    static func stub(url: URL, data: Data?, response:URLResponse?, error: Error? = nil) {
-        stubs[url] = Stub(data: data, response: response, error: error)
+    static func stub(data: Data?, response:URLResponse?, error: Error? = nil) {
+       stub = Stub(data: data, response: response, error: error)
     }
     
     static func startInterceptingRequests(){
@@ -78,12 +78,11 @@ private class URLProtocolStub: URLProtocol{
     
     static func stopInterceptingRequests(){
         URLProtocol.unregisterClass(URLProtocolStub.self)
+        stub = nil
     }
     
     override class func canInit(with request: URLRequest) -> Bool {
-        guard let url = request.url else { return false}
-        
-        return URLProtocolStub.stubs[url] != nil
+        return true
     }
     
     override class func canonicalRequest(for request: URLRequest) -> URLRequest {
@@ -91,7 +90,7 @@ private class URLProtocolStub: URLProtocol{
     }
     
     override func startLoading() {
-        guard let url = request.url, let stub = URLProtocolStub.stubs[url] else {
+        guard let stub = URLProtocolStub.stub else {
             return
         }
         
