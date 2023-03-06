aade5d748ca8458ef70669ae4a6a8fe3deecc6fc
Wan Lutfi <lutfime_2000@yahoo.com>
2023-03-06 14:34:34 +0800

    Move `ErrorView` to separate file

---
+++ EssentialFeediOS/Feed UI/Views/ErrorView.swift
@@ -0,0 +1,12 @@
+//
+//  ErrorView.swift
+//  EssentialFeediOS
+//
+//  Created by Wan Ahmad Lutfi on 06/03/2023.
+//
+
+import UIKit
+
+public final class ErrorView: UIView {
+    public var message: String?
+}
