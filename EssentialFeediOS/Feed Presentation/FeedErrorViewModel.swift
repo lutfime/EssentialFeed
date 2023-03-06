bc3d625b93fff1862bf8f955850241fc938b6417
Wan Lutfi <lutfime_2000@yahoo.com>
2023-03-06 14:45:42 +0800

    Move `FeedErrorViewModel` to separate file

---
+++ EssentialFeediOS/Feed Presentation/FeedErrorViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedErrorViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Wan Ahmad Lutfi on 06/03/2023.
+//
+
+import Foundation
+
+struct FeedErrorViewModel {
+    let message: String?
+}
