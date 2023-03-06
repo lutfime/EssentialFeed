31d6212c55e4bb8cd28ad633be88318916da86ea
Wan Lutfi <lutfime_2000@yahoo.com>
2023-03-06 15:09:35 +0800

    Move `FeedViewModel` to separate file

---
+++ EssentialFeed/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeed
+//
+//  Created by Wan Ahmad Lutfi on 06/03/2023.
+//
+
+import Foundation
+
+public struct FeedViewModel {
+    public let feed: [FeedImage]
+}
