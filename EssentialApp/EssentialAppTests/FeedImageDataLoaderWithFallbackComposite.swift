daaab620b11bdc99c3f7a6e4846a74ec652d8625
Wan Lutfi <lutfime_2000@yahoo.com>
2023-03-07 14:36:00 +0800

    `FeedImageDataLoaderWithFallbackComposite.loadImageData` cancels fallback loader task on cancel after a primary loader failure

--- EssentialApp/EssentialAppTests/FeedImageDataLoaderWithFallbackComposite.swift
+++ EssentialApp/EssentialAppTests/FeedImageDataLoaderWithFallbackComposite.swift
@@ -35,7 +35,7 @@ class FeedImageDataLoaderWithFallbackComposite: FeedImageDataLoader {
                 break
                 
             case .failure:
-                _ = self?.fallback.loadImageData(from: url) { _ in }
+                task.wrapped = self?.fallback.loadImageData(from: url) { _ in }
             }
 
         }
@@ -85,6 +85,17 @@ class FeedImageDataLoaderWithFallbackCompositeTests: XCTestCase {
         XCTAssertTrue(fallbackLoader.cancelledURLs.isEmpty, "Expected no cancelled URLs in the fallback loader")
     }
         
+    func test_cancelLoadImageData_cancelsFallbackLoaderTaskAfterPrimaryLoaderFailure() {
+        let url = anyURL()
+        let (sut, primaryLoader, fallbackLoader) = makeSUT()
+
+        let task = sut.loadImageData(from: url) { _ in }
+        primaryLoader.complete(with: anyNSError())
+        task.cancel()
+        
+        XCTAssertTrue(primaryLoader.cancelledURLs.isEmpty, "Expected no cancelled URLs in the primary loader")
+        XCTAssertEqual(fallbackLoader.cancelledURLs, [url], "Expected to cancel URL loading from fallback loader")
+    }
         
     // MARK: - Helpers
     
