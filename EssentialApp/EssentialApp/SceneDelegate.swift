eea85951788520bfc6e7117c2847534b412a56dc
Wan Lutfi <lutfime_2000@yahoo.com>
2023-03-10 12:32:33 +0800

    Make RemoteLoader conform to FeedLoader in the Composition Root

--- EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialApp/EssentialApp/SceneDelegate.swift
@@ -81,3 +81,5 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         localFeedLoader.validateCache { _ in }
     }
 }
+
+extension RemoteLoader: FeedLoader where Resource == [FeedImage] {}
