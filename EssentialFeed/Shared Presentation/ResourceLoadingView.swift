d10f6b2390b6a582c569e0f6d2fafeb424b753d0
Wan Lutfi <lutfime_2000@yahoo.com>
2023-03-11 21:42:57 +0800

    Rename and move ResourceLoadingView and VIewModel to Shared module

---
+++ EssentialFeed/Shared Presentation/ResourceLoadingView.swift
@@ -0,0 +1,12 @@
+//
+//  ResourceLoadingView.swift
+//  EssentialFeed
+//
+//  Created by Wan Ahmad Lutfi on 11/03/2023.
+//
+
+import Foundation
+
+public protocol ResourceLoadingView {
+    func display(_ viewModel: ResourceLoadingViewModel)
+}
