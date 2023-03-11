b617c57db6c8177c770374d223a8a638028748ed
Wan Lutfi <lutfime_2000@yahoo.com>
2023-03-11 21:37:14 +0800

    Move "GENERIC_CONNECTION_ERROR" localization key to new Shared.strings

--- EssentialFeed/Shared Presentation/LoadResourcePresenter.swift
+++ EssentialFeed/Shared Presentation/LoadResourcePresenter.swift
@@ -21,10 +21,10 @@ public final class LoadResourcePresenter<Resource, View: ResourceView> {
     private let errorView: FeedErrorView
     private let mapper: Mapper
     
-    private var feedLoadError: String {
+    static public var loadError: String {
         return NSLocalizedString("GENERIC_CONNECTION_ERROR",
-                tableName: "Feed",
-                bundle: Bundle(for: FeedPresenter.self),
+                tableName: "Shared",
+                bundle: Bundle(for: Self.self),
                 comment: "Error message displayed when we can't load the image feed from the server")
     }
     
@@ -46,7 +46,7 @@ public final class LoadResourcePresenter<Resource, View: ResourceView> {
     }
     
     public func didFinishLoading(with error: Error) {
-        errorView.display(.error(message: feedLoadError))
+        errorView.display(.error(message: Self.loadError))
         loadingView.display(FeedLoadingViewModel(isLoading: false))
     }
 }
