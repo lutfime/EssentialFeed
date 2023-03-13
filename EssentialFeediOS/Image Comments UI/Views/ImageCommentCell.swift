3050ecb57a0cf264c20844c0202b5c214266cead
Wan Lutfi <lutfime_2000@yahoo.com>
2023-03-13 09:35:09 +0800

    Implement Image Comments UI

---
+++ EssentialFeediOS/Image Comments UI/Views/ImageCommentCell.swift
@@ -0,0 +1,15 @@
+//
+//  ImageCommentCell.swift
+//  EssentialFeediOS
+//
+//  Created by Wan Ahmad Lutfi on 13/03/2023.
+//
+
+import UIKit
+
+public final class ImageCommentCell: UITableViewCell {
+    @IBOutlet private(set) public var messageLabel: UILabel!
+    @IBOutlet private(set) public var usernameLabel: UILabel!
+    @IBOutlet private(set) public var dateLabel: UILabel!
+}
+
