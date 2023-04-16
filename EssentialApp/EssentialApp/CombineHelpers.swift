c9840c71267888ad2bf6ec1468056aaf10d24b4c
Wan Lutfi <lutfime_2000@yahoo.com>
2023-04-16 14:17:52 +0800

    Make FeedImageDataCache sync

--- EssentialApp/EssentialApp/CombineHelpers.swift
+++ EssentialApp/EssentialApp/CombineHelpers.swift
@@ -159,7 +159,7 @@ extension DispatchQueue {
 
 extension FeedImageDataCache {
     func saveIgnoringResult(_ data: Data, for url: URL) {
-        save(data, for: url) { _ in }
+        try? save(data, for: url)
     }
 }
 
